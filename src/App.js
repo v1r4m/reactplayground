73b0df35693be30fee32e1a2f1f9065cbd87df0d
v1r4m <choyoung7789@gmail.com>
2022-10-07 10:31:30 +0900

    first play / props.children

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Hello from './Hello';
+import './App.css';
+import Wrapper from './Wrapper';
+
+function App() {
+  return (
+    <Wrapper>
+      <Hello name="react" color="red"/>
+      <Hello color="pink"/>
+    </Wrapper>
+  );
+}
+
+export default App;
